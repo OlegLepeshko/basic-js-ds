fd6510d716a95d390beb6cae8608aae7b5d89c2c
OlegLepeshko <74596871+OlegLepeshko@users.noreply.github.com>
2022-10-13 10:14:57 +0300

    feat: add two tasks

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,23 +13,74 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
+
+
+// class Node {
+//   constructor(value) {
+//      this.value = value
+//      this.next = null
+//    }
+//  }
+
+
+ class Queue {
+  constructor() {
+    this.root = null
+    this.tail = null
+    this.size = 0
+  }
+
+  isEmpty() {
+    return !this.size
+  }
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.root
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(item) {
+    // Create node
+    const newNode = new ListNode(item)
+    /**
+     * * If our list is empty than both our
+     * *.root item and tail item is going to point the new node.
+     */
+    if (this.isEmpty()) {
+      this.root = newNode
+      this.tail = newNode
+    }
+    else {
+      this.tail.next = newNode
+      this.tail = newNode
+    }
+    this.size++
+    return this
   }
-
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+    //* if our queue is empty we return null
+    if (this.isEmpty()) return null
+    const itemToBeRemoved = this.root
+
+    /**
+     * * if both our.root and tail node are pointing the same item
+     * * we dequeued our tail node.
+     */
+    if (this.root === this.tail) {
+      this.tail = null
+    }
+    this.root = this.root.next
+    this.size--
+    return itemToBeRemoved.value
   }
 }
+
+const queue = new Queue();
+  queue.enqueue(5);
+  queue.enqueue(6);
+  queue.enqueue(7);
+  console.log(queue.dequeue());
+  console.log(queue.dequeue());
 
 module.exports = {
   Queue
